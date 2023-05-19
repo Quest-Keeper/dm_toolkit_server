a118cd898d5e2672ab1bae129e8d1d13f87d69a9
Harrison Ryan <hwryan12@gmail.com>
2023-05-19 09:56:24 -0500

    refactor: Update how monsters is serialized

--- app/controllers/api/v1/monsters_controller.rb
+++ app/controllers/api/v1/monsters_controller.rb
@@ -1,12 +1,32 @@
 class Api::V1::MonstersController < ApplicationController
-  def index
-    service = Open5eApiService.new
-    monsters = service.get_monsters(params[:type])
-    
-    challenge_rating = params[:challenge_rating].to_i
-    filtered_monsters = monsters[:results].select { |monster| monster[:challenge_rating].to_i == challenge_rating }
-    monsters[:results] = filtered_monsters
+  # def index
+  #   service = Open5eApiService.new
+  #   monsters = service.get_monsters(params[:type])
 
-    render json: monsters
-  end
+  #   challenge_rating = params[:challenge_rating].to_i
+  
+  #   filtered_monsters = monsters[:results].select { |monster| monster[:challenge_rating].to_i == challenge_rating }
+
+  #   monsters[:results] = filtered_monsters
+  #   render json: monsters
+  # end
+  def index
+    data = Open5eApiService.new.get_monsters(params[:type])
+    challenge_rating = params[:challenge_rating].to_f
+    
+    data[:results].select! do |monster|
+  
+      monster[:challenge_rating].to_f == challenge_rating
+    end
+    data[:results].map! do |monster|
+      {
+        name: monster[:name],
+        size: monster[:size],
+        type: monster[:type],
+        challenge_rating: monster[:challenge_rating]
+      }
+    end
+
+    render json: data
+  end  
 end
